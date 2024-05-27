f5c5edc9f35f77ddf5c541808f14697ce28005dd
basat maqsood <itzbasatmaqsood@gmail.com>
2024-05-27 13:33:15 +0500

    second commit

--- src/components/RandomQuote/RandomQuote.jsx
+++ src/components/RandomQuote/RandomQuote.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
-import './RandomQuote.css'
+import React from "react";
+import "./RandomQuote.css";
+import twiter from "../assets/x.png";
+import cycle from "../assets/cycle.png";
 
 export default function RandomQuote() {
+  const [quote, setQuote] = React.useState({
+    text: "The only way to do great work is to love what you do.",
+    author: "Steve Jobs",
+  });
+
   return (
-    <div>RandomQuote</div>
-  )
+    <div className="container">
+      <div className="quote">{quote.text}</div>
+      <div className="bottom">
+        <div className="author">{quote.author}</div>
+        <div className="icons">
+          <img src={cycle} alt="" />
+          <img src={twiter} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 }
