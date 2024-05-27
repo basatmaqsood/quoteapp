63d7d61ad4ee7da7531e3fd4b85303c46aeabd8f
basat maqsood <itzbasatmaqsood@gmail.com>
2024-05-27 14:31:11 +0500

    final

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import RandomQuote from "./components/RandomQuote/RandomQuote";
 
-
 function App() {
   return (
     <div>
-<RandomQuote />
-<h6 className="tagline">Developed with Love By Basat Maqsood</h6>
+      <RandomQuote />{" "}
     </div>
   );
 }
