8e3ed3a837f41296884766b852856ca09c022e19
basat maqsood <itzbasatmaqsood@gmail.com>
2024-05-27 14:13:52 +0500

    finished

--- src/components/RandomQuote/RandomQuote.jsx
+++ src/components/RandomQuote/RandomQuote.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./RandomQuote.css";
 import twiter from "../assets/x.png";
 import cycle from "../assets/cycle.png";
+import { quotes } from "./quotes";
 
 export default function RandomQuote() {
-  const [quote, setQuote] = React.useState({
+
+    function random(){
+        let randomIndex = Math.floor(Math.random() * quotes.length);
+        setQuote( quotes[randomIndex]);
+    }
+
+    function twitterpost(){
+        window.open(`https://twitter.com/intent/tweet?text=${quote.text} - ${quote.author}`);
+    }
+  const [quote, setQuote] = useState({
     text: "The only way to do great work is to love what you do.",
     author: "Steve Jobs",
   });
@@ -13,10 +23,10 @@ export default function RandomQuote() {
     <div className="container">
       <div className="quote">{quote.text}</div>
       <div className="bottom">
-        <div className="author">{quote.author}</div>
+        <div className="author">{quote.author.split(',')[0]}</div>
         <div className="icons">
-          <img src={cycle} alt="" />
-          <img src={twiter} alt="" />
+          <img src={cycle} onClick ={()=>random()}alt="" />
+          <img src={twiter} onClick={()=> twitterpost()} alt="" />
         </div>
       </div>
     </div>
